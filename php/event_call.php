036ebafe1922f373ce21c8ac43f8dd35b359a05e
MathieuBro <mathieu-brossard@caramail.com>
2015-03-04 13:22:59 +0100

    upadte recup event
    Update des fichiers php récupérants les évènements

--- php/event_call.php
+++ php/event_call.php
@@ -1,4 +1,37 @@
 <?php
 
+include_once("connexion.inc");
 
+	$requete = "SELECT * FROM event WHERE login = '".$login."'";
+	
+	try {
+	$response = $connexion->prepare($requete);
+	$response->execute();
+	}
+	catch (Exception $e) {
+		echo $e->getMessage();
+	}
+	
+	$data = $response->fetchAll();
+
+
+	$json_response = array();
+
+	foreach($data as $row) {
+		$row_array['name'] = $row['name'];
+		$row_array['date_debut'] = $row['date_debut'];
+		$row_array['duration'] = $row['duration'];
+		$row_array['location'] = $row['location'];
+		$row_array['preview'] = $row['preview'];
+		$row_array['hour'] = $row['hour'];
+		$row_array['campus'] = $row['campus'];
+	}
+		//push the values in the array
+		array_push($json_response,$row_array);
+	}
+
+	echo json_encode($json_response);
+
+	fclose($db);
+			
 ?>
