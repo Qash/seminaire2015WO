5dbb6c9e087935c56969721481c67b5d197faab3
MathieuBro <mathieu-brossard@caramail.com>
2015-03-04 16:17:13 +0100

    16h16
    Je savais pas quoi mettre et j'ai trouvé l'heure plutôt cool

---
+++ php/event_inscription.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+$_SESSION['id_user'] = 11302484;
+	include_once("connexion.inc");
+
+if(isset($_SESSION['id_user'])) {
+		$requete = "SELECT * FROM user where id_user = '".$_SESSION['id_user']."'";
+	
+	try {
+	$response = $connexion->prepare($requete);
+	$response->execute();
+	}
+	catch (Exception $e) {
+		echo $e->getMessage();
+	}
+	
+	$data = $response->fetchAll();
+
+
+	$row_array = array();
+	$json_response = array();
+
+	foreach($data as $row) {
+		$user = $row['id_user'];
+		$sc = $row['mem_sc'];
+		if ($sc == 1) {
+			$sc = 'oui';
+		}
+		else if($sc ==0) {
+			$sc = 'non';
+		}
+		else {
+			echo ('erreur sur '.$sc);
+		}
+	}
+
+	if($sc == 'oui') {
+		if(isset($_POST['send']) && isset($_POST['event'])) {
+
+			$req = "INSERT INTO  `11402421-seminaire2015WO`.`RelSubscribe` (`user` ,`event`)VALUES ('".$user."',  '".$event."')";
+
+		} else {
+			echo('Vous avez essayé de vous conencter sans passer par la voix normale');
+		}
+	} else {
+		echo ("Vous n'êtes pas inscrits au service culturel");
+	}
+}
+
+?>
