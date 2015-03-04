d2a229f88028f5127901624f8db4ea65e2ee84a5
MathieuBro <mathieu-brossard@caramail.com>
2015-03-05 00:59:15 +0100

    Update event_call.php

--- php/event_call.php
+++ php/event_call.php
@@ -21,6 +21,7 @@ include_once("connexion.inc");
 
 	foreach($data as $row) {
 		 $row_array['name'] = $row['name'];
+		  $row_array['date_debut'] = $row['date_debut'];
 		 $json_response[] = $row_array; 
 	}
 	
