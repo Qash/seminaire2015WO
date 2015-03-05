0cd8bd9082f34f0c0a6ff7ae633999795074147f
MathieuBro <mathieu-brossard@caramail.com>
2015-03-05 13:19:40 +0100

    Update inscription_newsletter.php

--- php/inscription_newsletter.php
+++ php/inscription_newsletter.php
@@ -2,8 +2,33 @@
 if(isset($_POST['submit'])){
 	if(!empty($_POST['firstname']) && !empty($_POST['lastname']) && !empty($_POST['mail'])){
 		if(!empty($_POST['category'])){
-			foreach($_POST['category'] as $cat) {
-				echo $cat;
+			$firstname = $_POST['firstname'];
+			$lastname = $_POST['lastname'];
+			$mail = $_POST['mail'];
+			
+			$requete = "SELECT mail FROM `newsletter`";
+			try {
+				$response = $connexion->prepare($requete);
+				$response->execute();
+			}
+			catch (Exception $e) {
+				echo $e->getMessage();
+			}
+			
+			$data = $response->fetchAll();
+			$row_array = array();
+			
+			foreach($data as $row) {
+				$row_array['mail'] = $row['mail'];
+				if ($row_array['mail'] == $mail) {
+					$mail_exists = true;
+				}
+			}
+			
+			if(!$mail_exists){
+				foreach($_POST['category'] as $cat) {
+					echo $cat;
+				}
 			}
 		} else {
 			echo ("Aucune case selectionné ");
