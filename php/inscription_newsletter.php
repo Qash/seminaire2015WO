007b8dee08352903f43c9c45b16c956d8349734f
MathieuBro <mathieu-brossard@caramail.com>
2015-03-05 12:13:08 +0100

    Update inscription_newsletter.php

--- php/inscription_newsletter.php
+++ php/inscription_newsletter.php
@@ -1,5 +1,26 @@
 <?php
 session_start();
 
+include_once("connexion.inc");
+
+
+$requete = "SELECT * FROM `newsletter`";
+try {
+	$response = $connexion->prepare($requete);
+	$response->execute();
+}
+catch (Exception $e) {
+	echo $e->getMessage();
+}
+$data = $response->fetchAll();
+$row_array = array();
+$mail_array = array();
+
+foreach($data as $row) {
+	$row_array['mail'] = $row['mail'];
+	$mail_array[] = $row_array;
+}
+
+print_r ($mail_array);
 
 ?>
