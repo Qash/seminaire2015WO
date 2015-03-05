f2ae61ac8ab843308e531c95a14d0eadd09783d6
MathieuBro <mathieu-brossard@caramail.com>
2015-03-05 13:10:11 +0100

    Update inscription_newsletter.php

--- php/inscription_newsletter.php
+++ php/inscription_newsletter.php
@@ -1,45 +1,17 @@
 <?php
-
-session_start();
-
-if(isset($_POST['submit'])) {
-  if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['mail'])) {
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $mail = $_POST['mail'];
-    	
-    $requete = "SELECT mail FROM `newsletter`";
-    try {
-      $response = $connexion->prepare($requete);
-      $response->execute();
-    }
-    catch (Exception $e) {
-      echo $e->getMessage();
-    }
-    
-    $data = $response->fetchAll();
-    $row_array = array();
-    
-    foreach($data as $row) {
-      $row_array['mail'] = $row['mail'];
-      if($row_array['mail'] == $mail) {
-        $mail_exists = true;
-      }
-    }
-    
-    if($mail_exists === true) {
-      echo ("Le mail ".$mail." a déjà été utilisé");
-    } else {
-      foreach ($_POST['category'] as $cat) {
-        echo $cat;
-      }
-    }
-    
-    
-  } else {
-    echo ("Veuillez remplir le formulaire svp");
-  }
+if(isset($_POST['submit'])){
+	if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['mail'])){
+		if(!empty($_POST['category'])){
+			foreach($_POST['category'] as $cat) {
+				echo $cat;
+			}
+		} else {
+			echo ("Aucune case selectionné ");
+		}
+	} else {
+		echo ("Tout les champs n'ont pas été remplis");
+	}
 } else {
-  echo ("Le formulaire n'a pas été remplis");
+	echo ("Vous n'êtes pas passés par le formulaire, comment avez vous fait ?");
 }
 ?>
