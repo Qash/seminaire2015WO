70a66300996d99ca5b644c9c49bf0ef5df53207c
MathieuBro <mathieu-brossard@caramail.com>
2015-03-05 13:29:13 +0100

    Update inscription_newsletter.php

--- php/inscription_newsletter.php
+++ php/inscription_newsletter.php
@@ -6,24 +6,7 @@ if(isset($_POST['submit'])){
 			$lastname = $_POST['lastname'];
 			$mail = $_POST['mail'];
 			
-			$requete = "SELECT mail FROM `newsletter`";
-			try {
-				$response = $connexion->prepare($requete);
-				$response->execute();
-			}
-			catch (Exception $e) {
-				echo $e->getMessage();
-			}
-			
-			$data = $response->fetchAll();
-			$row_array = array();
-			
-			foreach($data as $row) {
-				$row_array['mail'] = $row['mail'];
-				if ($row_array['mail'] == $mail) {
-					$mail_exists = true;
-				}
-			}
+
 			
 			if($mail_exists !== true){
 				foreach($_POST['category'] as $cat) {
