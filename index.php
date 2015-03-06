83382d736b15647c2a8e440bccff85ff3754fa79
MathieuBro <mathieu-brossard@caramail.com>
2015-03-06 01:09:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -123,6 +123,7 @@ $_SESSION['id_user'] = 11302484;
 									<?php if(isset($_SESSION['id_user'])) {
 									?>
 									<form action="php/event_inscription.php" method="POST">
+										<input type="text" name="event" value="{{event.name}}">
 										<input type="submit" name="submit" value="inscription">
 									</form>
 									<?php
