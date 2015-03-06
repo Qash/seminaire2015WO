dfcc3f5021f12f638ad3ef3fe8d51b4362058a31
MathieuBro <mathieu-brossard@caramail.com>
2015-03-06 03:40:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ $_SESSION['id_user'] = 11302484;
 								<img src="{{event.preview}}" alt="img of {{event.name}}">
 								<h2>{{event.name}}</h2>
 								<p class="date" class="eventDate">{{event.date_debut | date : 'd-MMMM-yyyy'}}</p>
-								<p class="description">Description : <br />{{event.description}}</p>
+								<p class="description"><span class="desc">Description : </span><br />{{event.description}}</p>
 								<?php if(isset($_SESSION['id_user'])) {
 								?>
 								<form action="php/event_inscription.php" method="POST">
@@ -103,7 +103,7 @@ $_SESSION['id_user'] = 11302484;
 									<img src="{{event.preview}}" alt="img of {{event.name}}">
 									<h2>{{event.name}}</h2>
 									<p class="date" class="eventDate">{{event.date_debut | date : 'd/MMM/yyyy'}}</p>
-									<p class="description">Description : <br />{{event.description}}</p>
+									<p class="description"><span class="desc">Description :</span> <br />{{event.description}}</p>
 									<?php if(isset($_SESSION['id_user'])) {
 									?>
 									<form action="php/event_inscription.php" method="POST">
@@ -121,7 +121,7 @@ $_SESSION['id_user'] = 11302484;
 									<img src="{{event.preview}}" alt="img of {{event.name}}">
 									<h2>{{event.name}}</h2>
 									<p class="date" class="eventDate">{{event.date_debut | date : 'd-MMMM-yyyy'}}</p>
-									<p class="description">Description : <br />{{event.description}}</p>
+									<p class="description"><span class="desc">Description :</span> <br />{{event.description}}</p>
 									<?php if(isset($_SESSION['id_user'])) {
 									?>
 									<form action="php/event_inscription.php" method="POST">
